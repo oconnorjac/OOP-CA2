6b7748d299b6bca4646ad7e3a9f025bdd8d193ae
jacqueline <D00233669@student.dkit.ie>
2020-11-30 13:54:26 +0000

    final commit, ran out of time

--- src/main/java/com/dkit/oop/App.java
+++ src/main/java/com/dkit/oop/App.java
@@ -17,8 +17,7 @@ public class App
         app.start();
     }
 
-    public void start()
-    {
+    public void start() {
         System.out.println("Q1 - some simple tests");
         ArrayList<String> facilities = new ArrayList<String>();
 
@@ -27,11 +26,11 @@ public class App
         System.out.println("Property p1 details:");
         System.out.println(p1.getPropertyID() + " : " + p1.getOwner() + " : " + p1.getPostcode() + " : " + p1.getSellingPrice() + " : " + p1.getArea());
 
-        Property p2 = new Property(2,"John Smith","A4RD7T7",265000,250,facilities);
+        Property p2 = new Property(2, "John Smith", "A4RD7T7", 265000, 250, facilities);
 
-        Property p3 = new Property(3,"Jack Attack","A92ERF",585694,350,facilities);
+        Property p3 = new Property(3, "Jack Attack", "A92ERF", 585694, 350, facilities);
         Property p4 = p3;
-        Property p5 = new Property(3,"Jack Attack","A92ERF",585694,350,facilities);
+        Property p5 = new Property(3, "Jack Attack", "A92ERF", 585694, 350, facilities);
         Property p6 = null;
 
         // part 3 and 4 tests
@@ -40,18 +39,18 @@ public class App
         p2.addFacility("electricity");
         p2.addFacility("freehold");
         p2.addFacility("private access");
-        System.out.println("p1s facilities: "+p1.getFacilities());
-        System.out.println("p2s facilities: "+p2.getFacilities());
+        System.out.println("p1s facilities: " + p1.getFacilities());
+        System.out.println("p2s facilities: " + p2.getFacilities());
 
         //part 5 tests
-        System.out.println("p3==p4: "+(p3==p4));
-        System.out.println("p3.equals(p4)"+p3.equals(p4));
-        System.out.println("p3==p5: "+(p3==p5));
-        System.out.println("p3.equals(p5)"+p3.equals(p5));
+        System.out.println("p3==p4: " + (p3 == p4));
+        System.out.println("p3.equals(p4)" + p3.equals(p4));
+        System.out.println("p3==p5: " + (p3 == p5));
+        System.out.println("p3.equals(p5)" + p3.equals(p5));
 
         //part 6
         double tax = p1.calculateTax();
-        System.out.println("tax on p1: "+tax);
+        System.out.println("tax on p1: " + tax);
 
         System.out.println("Q2 - tests");
         ArrayList<Property> properties = new ArrayList<>();
@@ -63,20 +62,20 @@ public class App
         displayAllProperties(properties);
 
         System.out.println("\nSELLING ABOVE");
-        ArrayList<Property> sellingAbove = getPropertiesPriceAbove(properties,260000);
+        ArrayList<Property> sellingAbove = getPropertiesPriceAbove(properties, 260000);
         displayAllProperties(sellingAbove);
 
 
         System.out.println("\n\nQ3 - tests");
-        PrivateProperty pp1 = new PrivateProperty(8, "Joe Smith", "A98AB82", 285000, 130, facilities,5,PropertyType.APARTMENT);
+        PrivateProperty pp1 = new PrivateProperty(8, "Joe Smith", "A98AB82", 285000, 130, facilities, 5, PropertyType.APARTMENT);
         System.out.println(pp1);
         properties.add(pp1);
-        PrivateProperty pp2 = new PrivateProperty(9, "Mike Smith", "A98TG82", 250000, 420, facilities,3,PropertyType.SEMI_D);
-        PrivateProperty pp3 = new PrivateProperty(10, "Joe Bloggs", "A944B8G", 310000, 190, facilities,2,PropertyType.DETACHED);
+        PrivateProperty pp2 = new PrivateProperty(9, "Mike Smith", "A98TG82", 250000, 420, facilities, 3, PropertyType.SEMI_D);
+        PrivateProperty pp3 = new PrivateProperty(10, "Joe Bloggs", "A944B8G", 310000, 190, facilities, 2, PropertyType.DETACHED);
         properties.add(pp2);
         properties.add(pp3);
-        PrivateProperty pp4 = new PrivateProperty(11, "Bob Black", "A9THB8G", 275000, 308, facilities,4,PropertyType.DETACHED);
-        PrivateProperty pp5 = new PrivateProperty(12, "Jane Brown", "YDFB8G", 190000, 280, facilities,5,PropertyType.DETACHED);
+        PrivateProperty pp4 = new PrivateProperty(11, "Bob Black", "A9THB8G", 275000, 308, facilities, 4, PropertyType.DETACHED);
+        PrivateProperty pp5 = new PrivateProperty(12, "Jane Brown", "YDFB8G", 190000, 280, facilities, 5, PropertyType.DETACHED);
         properties.add(pp4);
         properties.add(pp5);
 
@@ -89,34 +88,35 @@ public class App
         // PrivateProperty type
 
         System.out.println("\nPART 6 = ID AND TAX : ");
-        System.out.println("ID : "+p1.getPropertyID()+" Tax: "+p1.calculateTax());
-        System.out.println("ID : "+pp3.getPropertyID()+" Tax: "+pp3.calculateTax());
+        System.out.println("ID : " + p1.getPropertyID() + " Tax: " + p1.calculateTax());
+        System.out.println("ID : " + pp3.getPropertyID() + " Tax: " + pp3.calculateTax());
 
         System.out.println("\n\nQ4 - tests");
         displayAllProperties(properties);
 
         System.out.println("\n\nDISPLAY PROP ID & TAX ");
         for (Property p : properties) {
-            System.out.print("Property ID "+p.getPropertyID() + "\t");
-            System.out.format("Tax :"+p.calculateTax()+"\n");
+            System.out.print("Property ID " + p.getPropertyID() + "\t");
+            System.out.format("Tax :" + p.calculateTax() + "\n");
             //if (p instanceof PrivateProperty) {
-           //     System.out.print("Property ID "+p.getPropertyID() + "\t");
+            //     System.out.print("Property ID "+p.getPropertyID() + "\t");
             //    PrivateProperty pp = (PrivateProperty) p;
             //    System.out.format("Tax :"+pp.calculateTax()+"\n");
-           // }
+            // }
         }
 
         System.out.println("\n\n DISPLAY PROP ID, SELLING PRICE, NUMROOMS FOR DETACHED");
-        for(Property p : properties){
+        for (Property p : properties) {
 
-            if (p instanceof PrivateProperty)
-                if(((PrivateProperty) p).getType()==PropertyType.DETACHED)
-                System.out.println("ID: "+p.getPropertyID()+ " Price: "+p.getSellingPrice());
-                PrivateProperty pp = (PrivateProperty) p;
-                System.out.format("Rooms :"+pp.getNumBeds());
+            if (p instanceof PrivateProperty) {
+                if (((PrivateProperty) p).getType() == PropertyType.DETACHED) {
+                    System.out.println("\tID: " + p.getPropertyID() + " Price: " + p.getSellingPrice());
+                    PrivateProperty pp = (PrivateProperty) p;
+                    System.out.format("Rooms :" + pp.getNumBeds());
+                }
             }
-        }
 
+        }
     }
 
     public static void displayAllProperties(ArrayList<Property> p){
